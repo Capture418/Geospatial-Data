7fa906b195f923c348cd9bea3d42673b497e72da
Capture418 <98550584+Capture418@users.noreply.github.com>
2022-08-16 16:05:41 +0100

    Tourism / NUTS Merge and Map

---
+++ data/funcs.py
@@ -0,0 +1,28 @@
+def transform_data(df_tourism):
+
+    df_tourism['id'] = df_tourism['TIME'].astype(str) + '/' + df_tourism['GEO']
+    df_pivot = df_tourism.pivot(index = 'id', columns = ['ACCOMUNIT','UNIT'], values = 'Value')
+    new_names = ['_'.join(x) for x in df_pivot.columns]
+    df_pivot.columns = new_names
+    df_pivot['new_id'] = df_pivot.index.values
+    new = df_pivot['new_id'].str.split('/', expand = True)
+    df_pivot['nuts218nm'] = new[1].values
+    df_pivot['year'] = new[0].values
+    df_pivot.reset_index(inplace = True, drop = True)
+    df_pivot.drop(['Bedrooms_Number','Bedrooms_Percentage change on previous period', 'new_id'], inplace = True, axis = 1)
+    return df_pivot
+
+def call2016():
+    
+    import geopandas as gpd
+    import pandas as pd
+    import funcs as tourism
+    FILENAME1 = 'NUTS_Level_2_(January_2018)_Boundaries.geojson'
+    FILENAME2 = 'tour_cap_nuts2_1_Data.csv'
+    gdf = gpd.read_file(FILENAME1)
+    df_tourism = pd.read_csv(FILENAME2)
+    transformed_df = tourism.transform_data(df_tourism)
+    df_2016 = transformed_df[transformed_df['year']=='2016']
+    return df_2016
+
+
